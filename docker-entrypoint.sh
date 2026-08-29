5a3f8032bf8a2759913fcbbbee16d0111e9e871d
naoki747 <naoano32@proton.me>
2026-08-29 23:16:58 +0900

    Refactor entrypoint script for nginx configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,10 @@
 set -eu
 
 PORT="${PORT:-10000}"
-
 export PORT
 
-envsubst '${PORT}' \
-    < /etc/nginx/templates/render-nginx.conf.template \
-    > /etc/nginx/conf.d/default.conf
+envsubst '${PORT}' < /etc/nginx/templates/render-nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 nginx -t
 
-exec /usr/bin/supervisord \
-    -c /etc/supervisor
+exec /usr/bin/supervisord --nodaemon --configuration=/etc/supervisord.conf
